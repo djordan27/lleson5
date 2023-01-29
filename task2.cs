c724ac219287fb40c4cf366ae31532c3b3432955
Игорь <211353@mail.ru>
2023-01-29 20:39:32 +0300

    finish

---
+++ task2.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int [] array () {
+    int size = 0;
+    Console.WriteLine("введите размер вашего массива: ");
+    int.TryParse(Console.ReadLine(), out size);
+    int [] result = new int [size];
+    return result;
+}
+
+int [] arrayFilling(int[] array) {
+    Random rnd = new Random();
+    int count = 0;
+    Console.Write("[ ");
+    while(count < array.Length){
+        array[count] = rnd.Next(-2_147_483_648, 2_147_483_647);
+        Console.Write(array[count] + " ");
+        count++;
+    }
+    Console.Write("]");
+    return array;
+}
+
+int notEvenNumber (int[] array) {
+    int notEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0 && i != 0) {
+            notEven += array[i];
+        }
+    }
+    return notEven;
+}
+
+int [] myArray = array();
+myArray = arrayFilling(myArray);
+int notEven = notEvenNumber(myArray);
+Console.WriteLine($"\nСумма нечетных чисел: {notEven}");
---
+++ task3.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+double [] array () {
+    int size = 0;
+    Console.WriteLine("введите размер вашего массива: ");
+    int.TryParse(Console.ReadLine(), out size);
+    double [] result = new double [size];
+    return result;
+}
+
+double [] arrayFilling(double[] array) {
+    Random rnd = new Random();
+    int count = 0;
+    Console.Write("[ ");
+    while(count < array.Length){
+        array[count] = rnd.Next(100, 1000);
+        Console.Write(array[count] + " ");
+        count++;
+    }
+    Console.Write("]");
+    return array;
+}
+
+double difference (double[] array) {
+    double minNumber = array[0];
+    double maxNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(minNumber > array[i]) {
+            minNumber = array[i];
+        }
+        if(maxNumber < array[i]) {
+            maxNumber = array[i];
+        }
+    }
+
+    return maxNumber - minNumber;
+}
+
+double [] myArray = array();
+myArray = arrayFilling(myArray);
+double myDifference = difference(myArray);
+Console.WriteLine($"\nРазница: {myDifference}");
